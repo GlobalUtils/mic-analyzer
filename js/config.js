efbb27a82c18f000c4bde420eff0d7092c07ab60
GlobalUtils <freecalx@gmail.com>
2025-04-03 23:33:03 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// js/config.js
+export const FFT_SIZE = 2048;
+export const DEFAULT_THEME = 'dark';
+export const SUPPORTED_LANGUAGES = {
+    'en': 'English',
+    'es': 'Español',
+    'pt': 'Português',
+    'fr': 'Français',
+    'de': 'Deutsch',
+    'ar': 'العربية'
+};
+export const DEFAULT_LANG = 'en';
+export const LOCAL_STORAGE_THEME_KEY = 'micAnalyzerTheme';
+export const LOCAL_STORAGE_LANG_KEY = 'micAnalyzerLang';
